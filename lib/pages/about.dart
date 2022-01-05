2c5e5c955957e275816eb528d4e6c9b631c3d511
SincerelyFaust <hedesmarin@gmail.com>
2022-01-05 20:53:02 +0100

    Rewrite About card class as widget

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -15,10 +15,57 @@ limitations under the License.
 */
 
 import 'package:flutter/material.dart';
-import 'package:welcome/widgets/about_card.dart';
+import 'package:welcome/models/about_card_item_model.dart';
+import 'package:welcome/widgets/cards/about_card.dart';
 
 class About extends StatelessWidget {
   const About({Key? key}) : super(key: key);
+
+  static const _systemInformationList = <AboutCardItem>[
+    AboutCardItem(
+      'Operating system',
+      'dahliaOS',
+      Icons.computer,
+    ),
+    AboutCardItem(
+      'Architecture',
+      'ARMv8.4-A',
+      Icons.architecture,
+    ),
+    AboutCardItem(
+      'Build info',
+      '21XXXX',
+      Icons.build,
+    ),
+    AboutCardItem(
+      'Kernel version',
+      '5.11.1',
+      Icons.update,
+    ),
+  ];
+
+  static const _hardwareInformationList = <AboutCardItem>[
+    AboutCardItem(
+      'CPU',
+      'Apple M1',
+      Icons.settings_input_component,
+    ),
+    AboutCardItem(
+      'GPU',
+      'Apple M1',
+      Icons.settings_input_hdmi,
+    ),
+    AboutCardItem(
+      'RAM',
+      '8GB',
+      Icons.memory,
+    ),
+    AboutCardItem(
+      'Storage',
+      '512GB',
+      Icons.storage,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -72,35 +119,12 @@ class About extends StatelessWidget {
                 const SizedBox(
                   height: 10,
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    aboutCard(
-                      'Operating system',
-                      'dahliaOS',
-                      Icons.computer,
-                      context,
-                    ),
-                    aboutCard(
-                      'Architecture',
-                      'ARMv8.4-A',
-                      Icons.architecture,
-                      context,
-                    ),
-                    aboutCard(
-                      'Build info',
-                      '21XXXX',
-                      Icons.build,
-                      context,
-                    ),
-                    aboutCard(
-                      'Kernel version',
-                      '5.11.1',
-                      Icons.update,
-                      context,
-                    ),
-                  ],
-                ),
+                for (final item in _systemInformationList)
+                  AboutCard(
+                    item.name,
+                    item.description,
+                    item.icon,
+                  ),
                 const SizedBox(
                   height: 10,
                 ),
@@ -117,30 +141,12 @@ class About extends StatelessWidget {
                 const SizedBox(
                   height: 10,
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    aboutCard(
-                      'CPU',
-                      'Apple M1',
-                      Icons.settings_input_component,
-                      context,
-                    ),
-                    aboutCard(
-                      'GPU',
-                      'Apple M1',
-                      Icons.settings_input_hdmi,
-                      context,
-                    ),
-                    aboutCard('RAM', '8GB', Icons.memory, context),
-                    aboutCard(
-                      'Storage',
-                      '512GB',
-                      Icons.storage,
-                      context,
-                    ),
-                  ],
-                ),
+                for (final item in _hardwareInformationList)
+                  AboutCard(
+                    item.name,
+                    item.description,
+                    item.icon,
+                  ),
               ],
             ),
           ),
